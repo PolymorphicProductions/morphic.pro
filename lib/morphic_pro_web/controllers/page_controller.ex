b6a091b949b5e2ce36f9bc9da5aa859f50da218c
Josh Chernoff <jchernoff@morphic.pro>
2020-04-16 11:14:54 -0700

    add hover to thumbs

--- lib/morphic_pro_web/controllers/page_controller.ex
+++ lib/morphic_pro_web/controllers/page_controller.ex
@@ -2,7 +2,8 @@ defmodule MorphicProWeb.PageController do
   use MorphicProWeb, :controller
 
   def index(conn, _params) do
-    posts = MorphicPro.Blog.list_latest_posts
-    render(conn, "index.html", posts: posts)
+    posts = MorphicPro.Blog.list_latest_posts()
+    snaps = MorphicPro.Blog.list_latest_snaps()
+    render(conn, "index.html", posts: posts, snaps: snaps)
   end
 end
