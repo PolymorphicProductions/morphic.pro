befef9f0e5b19a222a82b735df1db97e0eb413fa
Josh Chernoff <jchernoff@morphic.pro>
2020-04-16 11:23:09 -0700

    add hover to thumbs (#36)

--- lib/morphic_pro_web/views/page_view.ex
+++ lib/morphic_pro_web/views/page_view.ex
@@ -3,7 +3,18 @@ defmodule MorphicProWeb.PageView do
 
   def parse_tags(tags) do
     Enum.map(tags, fn tag ->
-      content_tag(:span, link("##{tag.name}", to: Routes.post_tag_path(MorphicProWeb.Endpoint, :show_post, tag.name), class: "inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200"))
+      content_tag(
+        :span,
+        link("##{tag.name}",
+          to: Routes.post_tag_path(MorphicProWeb.Endpoint, :show_post, tag.name),
+          class: "inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200"
+        )
+      )
     end)
+  end
+
+  def parse_body_tags(content) do
+    content
+    |> String.replace(~r/#(\w*)/, "<a href='/snaps/tag/\\1'>#\\1</a>")
   end
 end
