fda6e3dbcde744c1c57fad2726246a96c1daefe9
Josh Chernoff <jchernoff@morphic.pro>
2020-04-23 10:02:28 -0700

    override lib to see path its looking for (#47)

---
+++ lib/capchta.ex
@@ -0,0 +1,21 @@
+defmodule Captcha do
+  # allow customize receive timeout, default: 10_000
+  def get(timeout \\ 1_000) do
+    path = Path.join(:code.priv_dir(:captcha), "captcha") |> IO.inspect()
+
+    Port.open({:spawn, path}, [:binary])
+
+    # Allow set receive timeout
+    receive do
+      {_, {:data, data}} ->
+        <<text::bytes-size(5), img::binary>> = data
+        {:ok, text, img}
+
+      other ->
+        other
+    after
+      timeout ->
+        {:timeout}
+    end
+  end
+end
