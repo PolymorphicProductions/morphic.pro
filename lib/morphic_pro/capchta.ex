c215c35d5562e1f7fe24f64eabd281488fee706d
Josh Chernoff <jchernoff@morphic.pro>
2020-04-23 11:08:55 -0700

    fix bug in captch

--- lib/morphic_pro/capchta.ex
+++ lib/morphic_pro/capchta.ex
@@ -1,21 +1,13 @@
 defmodule MorphicPro.Captcha do
   # allow customize receive timeout, default: 10_000
-  def get(timeout \\ 4_000) do
-    path = Path.join(:code.priv_dir(:captcha), "captcha") |> IO.inspect()
-
-    Port.open({:spawn, path}, [:binary])
-
-    # Allow set receive timeout
-    receive do
-      {_, {:data, data}} ->
+  def get(timeout \\ 1_000) do
+    case System.cmd(Application.app_dir(:captcha, "priv/captcha"), []) do
+      {data, 0} ->
         <<text::bytes-size(5), img::binary>> = data
         {:ok, text, img}
 
       other ->
-        other
-    after
-      timeout ->
-        {:timeout}
+        :error
     end
   end
 end
