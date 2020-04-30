288765470cd46d00d940f4ea3c747ca93cbef028
Josh Chernoff <jchernoff@morphic.pro>
2020-04-30 11:25:40 -0700

    upgrade render_existing

--- lib/morphic_pro_web/views/layout_view.ex
+++ lib/morphic_pro_web/views/layout_view.ex
@@ -1,15 +1,4 @@
 defmodule MorphicProWeb.LayoutView do
   # FYI: If you add any logic besure to unignore from coveralls.json
   use MorphicProWeb, :view
-
-  def render_existing_nested(
-        %{
-          :phoenix_template => phoenix_template,
-          :phoenix_view => phoenix_view
-        },
-        tag,
-        assigns
-      ) do
-    render_existing(phoenix_view, tag <> phoenix_template, assigns)
-  end
 end
