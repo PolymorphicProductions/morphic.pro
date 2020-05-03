87249cfb3fbb2dcc3d8baa0101a496724ed727b5
Josh Chernoff <jchernoff@morphic.pro>
2020-05-03 12:24:54 -0700

    upgrade render_existing (#67)

--- lib/morphic_pro_web.ex
+++ lib/morphic_pro_web.ex
@@ -36,7 +36,9 @@ defmodule MorphicProWeb do
         namespace: MorphicProWeb
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+
       import Dissolver.HTML
 
       # Include shared imports and aliases for views
