98441940ec8e9c7d3b81f12f509298782cd984d3
Josh Chernoff <jchernoff@morphic.pro>
2020-05-25 12:03:25 -0700

    purge unused css

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,6 +1,18 @@
 module.exports = ctx => {
   return {
     map: ctx.env === "dev" ? ctx.map : false,
-    plugins: [require("tailwindcss"), require("autoprefixer"), require('postcss-nested')]
+    plugins: [
+      require("tailwindcss"),
+      require("autoprefixer"),
+      require('postcss-nested'),
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          "../lib/morphic_pro_web/**/*.leex",
+          "../lib/morphic_pro_web/**/*.eex",
+
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+    ]
   };
 };
