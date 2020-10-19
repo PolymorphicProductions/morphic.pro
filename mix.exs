969797b97ac92931f87f73df0f4ce5dae53c0e28
Josh Chernoff <jchernoff@morphic.pro>
2020-10-18 22:37:05 -0700

    wip

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule MorphicPro.MixProject do
       {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.5"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, ">= 0.15.7"},
       {:phoenix_html, "~> 2.11"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.2", override: true},
