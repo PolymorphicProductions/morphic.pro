3f04b2463176b677d6391fe37d645c87e7791008
Josh Чернов <jchernoff@morphic.pro>
2020-12-16 13:02:15 -0800

    Style cleanup (#353)
    * Add stylelint
    
    * Update css structure

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,19 +1,6 @@
-module.exports = ctx => {
-  return {
-    map: ctx.env === "dev" ? ctx.map : false,
-    plugins: [
-      require('postcss-nested'),
-      require("tailwindcss"),
-      require("autoprefixer"),
-      // require('@fullhuman/postcss-purgecss')({
-      //   content: [
-      //     "./css/**/*.css",
-      //     "../lib/morphic_pro_web/**/*.leex",
-      //     "../lib/morphic_pro_web/**/*.eex",
-      //     "../lib/morphic_pro_web/**/*.ex"
-      //   ],
-      //   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-      // })
-    ]
-  };
-};
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
